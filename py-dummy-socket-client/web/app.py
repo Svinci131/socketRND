3cfe7353aafd1aba3b612c6647685fd030addf7a
Samantha Vinci <samanthavinci@Samanthas-MBP.hellobond.io>
2017-04-20 10:16:05 -0400

    delete unused routes

--- py-dummy-socket-client/web/app.py
+++ py-dummy-socket-client/web/app.py
@@ -4,7 +4,6 @@ import sys
 from flask import Flask, request, render_template, send_from_directory, session, jsonify
 from socketIO_client import SocketIO, BaseNamespace
 
-
 import logging
 logging.getLogger('socketIO-client').setLevel(logging.DEBUG)
 logging.basicConfig()
@@ -20,19 +19,6 @@ class Namespace(BaseNamespace):
 def hello_world():
   socketIO = SocketIO('10.1.133.238', 50, Namespace)
   socketIO.emit('connection name', { 'name': 'HELLO WORLD ROUTE' })
-  return render_template('index.html')
-
-@app.route('/testsubscriber', methods=['POST'])
-def recieve():
-  print ('hi', request)
-  return 'recieved'
-
-@app.route('/scanned', methods=['POST'])
-def recieve_scanned():
-  data = request.get_json()
-
-  socketio.emit('scanned',
-    data, namespace='/test')
   return render_template('index.html')
 
 
