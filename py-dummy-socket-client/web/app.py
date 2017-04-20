132b19bc791b2b4024484858e3b9f52addb0f76e
Samantha Vinci <samanthavinci@Samanthas-MBP.hellobond.io>
2017-04-20 12:36:10 -0400

    added thread for listening

--- py-dummy-socket-client/web/app.py
+++ py-dummy-socket-client/web/app.py
@@ -14,28 +14,19 @@ app = Flask(__name__)
 
 class Namespace(BaseNamespace):
     def on_connect(self):
+      print('-----------------------------', file=sys.stderr)
       print('[Connected]', file=sys.stderr)
+      print('-----------------------------', file=sys.stderr)
     def on_test(self, *args):
+      print('-----------------------------', file=sys.stderr)
       print('test', file=sys.stderr)
+      print('-----------------------------', file=sys.stderr)
     def on_new_user_response(self, *args):
+      print('-----------------------------', file=sys.stderr)
       print('new user', file=sys.stderr)
+      print('-----------------------------', file=sys.stderr)
 
-def on_connect():
-  print('-----------------------------')
-  print('connected!!!', file=sys.stderr)
-  print('-----------------------------')
 
-def on_new_user_response():
-  print('-----------------------------')
-  print('new user', file=sys.stderr)
-  print('-----------------------------')
-  socketIO.emit('connection name', { 'name': 'recieved new user event' })
-
-def on_test():
-  print('-----------------------------')
-  print('test', file=sys.stderr)
-  print('-----------------------------')
-  
 @app.route('/')
 def hello_world():
   print('hit home route', file=sys.stderr)
@@ -50,9 +41,6 @@ def test():
 
 if __name__ == '__main__':
   socketIO = SocketIO('10.1.133.238', 50, Namespace)
-  socketIO.on('new_user', on_new_user_response)
-  socketIO.on('connection name', on_new_user_response)
-  socketIO.on('test', on_test)
   t = threading.Thread(target=socketIO.wait)
   t.daemon = True
   t.start()
