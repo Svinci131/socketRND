6318f3912c111003f70cfab2b370e1abd4d52c86
Samantha Vinci <samanthavinci@Samanthas-MBP.hellobond.io>
2017-04-21 11:09:03 -0400

    added more helpful logging

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from __future__ import print_function # In python 2.7
 import json
+import random
 import sys
 from flask import Flask, request, render_template, send_from_directory, session, jsonify
 from socketIO_client import SocketIO, BaseNamespace, LoggingNamespace
@@ -14,34 +15,38 @@ app = Flask(__name__)
 
 class Namespace(BaseNamespace):
     def on_connect(self):
-      printToConsole('[Connected]', None)
+      self.socketId = random.randrange(0, 10000),
+      print('-----------------------------', file=sys.stderr)
+      print('Socket ID:', self.socketId, '[Connected]', file=sys.stderr)
+      print('-----------------------------', file=sys.stderr)
     def on_test_event_from_node(self, *args):
       print('-----------------------------', file=sys.stderr)
-      print('[Recieved test_event_from_node]', args, file=sys.stderr)
+      print('Socket ID:', self.socketId,'[Recieved test_event_from_node]', args, file=sys.stderr)
       print('-----------------------------', file=sys.stderr)
+      self.emit('new_connection', { 'name': self.socketId })
     def on_test(self, *args):
       print('-----------------------------', file=sys.stderr)
-      print('[Test]', args, file=sys.stderr)
+      print('Socket ID:', self.socketId, '[Test]', args, file=sys.stderr)
       print('-----------------------------', file=sys.stderr)
     def on_connection_confirmed(self, *args):
       print('-----------------------------', file=sys.stderr)
-      print('[Connection Confirmed]', args, file=sys.stderr)
+      print('Socket ID:', self.socketId, '[Connection Confirmed]', args, file=sys.stderr)
       print('-----------------------------', file=sys.stderr)
 
 
 @app.route('/')
 def hello_world():
   print('hit home route', file=sys.stderr)
-  socketIO.emit('new_connection', { 'name': 'HELLO WORLD ROUTE' })
   return 'home'
 
-@app.route('/test')
-def test():
-  print('hit test route', file=sys.stderr)
-  socketIO.emit('test', 'testing')
-  return 'test'
+# @app.route('/test')
+# def test():
+#   print('hit test route', file=sys.stderr)
+#   socketIO.emit('test', 'testing')
+#   return 'test'
 
 if __name__ == '__main__':
+  # for i in range(0, 2):
   socketIO = SocketIO('10.1.133.238', 50, Namespace)
   t = threading.Thread(target=socketIO.wait)
   t.daemon = True
