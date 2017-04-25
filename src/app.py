31fca5e0550f30f688a5a1e5234c175f8f1ca9cc
Samantha Vinci <samanthavinci@Samanthas-MBP.hellobond.io>
2017-04-25 16:56:18 -0400

    update createSocketConnection

--- src/app.py
+++ src/app.py
@@ -18,16 +18,12 @@ class bcolors:
   BLUE = '\033[94m'
   GREEN = '\033[92m'
 
-class NamespaceOne(BaseNamespace):
+class Namespace(BaseNamespace):
   def on_connect(self):
     print(bcolors.PURPLE + '-----------------------------', file=sys.stderr)
     print(bcolors.PURPLE + 'Name Space:', self.path, '[Connected]', file=sys.stderr)
     print(bcolors.PURPLE + '-----------------------------', file=sys.stderr)
     self.emit('new_connection', { 'name': self.path })
-  # def on_test(self, *args):
-  #   print(bcolors.BLUE + '-----------------------------', file=sys.stderr)
-  #   print(bcolors.BLUE + 'Name Space:', self.path, '[Test]', args, file=sys.stderr)
-  #   print(bcolors.BLUE + '-----------------------------', file=sys.stderr)
   def on_unique_event_response(self, *args):
     print(bcolors.BLUE + '-----------------------------', file=sys.stderr)
     print(bcolors.BLUE + 'Name Space:', self.path, 'UNIQUE EVENT', args, file=sys.stderr)
@@ -38,7 +34,7 @@ class NamespaceOne(BaseNamespace):
     print(bcolors.GREEN + '-----------------------------', file=sys.stderr)
     self.emit('unique_event', { 'name': self.path })
 
-class NamespaceTwo(BaseNamespace):
+class DefaultNamespace(BaseNamespace):
   def on_connect(self):
     print(bcolors.PURPLE + '-----------------------------', file=sys.stderr)
     print(bcolors.PURPLE + 'Name Space:', self.path, '[Connected]', file=sys.stderr)
@@ -55,23 +51,24 @@ def hello_world():
   print('hit home route', file=sys.stderr)
   return 'home'
 
-def createSocketConnection(Namespace):
-  socketIO = SocketIO('10.1.133.238', 140, Namespace)
+def createSocketConnection(name):
+  if (name == None):
+    socketIO = SocketIO('10.1.133.238', 140, DefaultNamespace)
+  else:
+    socketIO = SocketIO('10.1.133.238', 140)
+    namespace = socketIO.define(Namespace, name)
+
   t = threading.Thread(target=socketIO.wait)
   t.daemon = True
   t.start()
 
 if __name__ == '__main__':
   
-  createSocketConnection(NamespaceTwo)
+  createSocketConnection(None)
+
   for i in range(0, 3):
-    #event only to people in name space 
     name = '/robot'+str(i)
-    socketIO = SocketIO('10.1.133.238', 140)
-    namespaceOne = socketIO.define(NamespaceOne, name)
-    t = threading.Thread(target=socketIO.wait)
-    t.daemon = True
-    t.start()
+    createSocketConnection(name)
 
   app.run(host='0.0.0.0', port=9000, debug=True)
 
