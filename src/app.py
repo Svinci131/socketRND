8bba7d54f107f80a219661cdf09b7f8cb7ac4366
Samantha Vinci <samanthavinci@Samanthas-MBP.hellobond.io>
2017-04-25 16:51:02 -0400

    added colors

--- src/app.py
+++ src/app.py
@@ -13,53 +13,42 @@ import threading
 
 app = Flask(__name__)
 
+class bcolors:
+  PURPLE = '\033[95m'
+  BLUE = '\033[94m'
+  GREEN = '\033[92m'
 
 class NamespaceOne(BaseNamespace):
   def on_connect(self):
-    self.socketId = 'NameSpace1',
-    print('-----------------------------', file=sys.stderr)
-    print('Socket ID:', self.socketId, '[Connected]', file=sys.stderr)
-    print('-----------------------------', file=sys.stderr)
-    self.emit('new_connection', { 'name': self.socketId })
-  def on_test_event_from_node(self, *args):
-    print('-----------------------------', file=sys.stderr)
-    print('Socket ID: ', self.socketId, '(For All Clients)[Recieved test_event_from_node]', args, file=sys.stderr)
-    print('-----------------------------', file=sys.stderr)
-  def on_test(self, *args):
-    print('-----------------------------', file=sys.stderr)
-    print('Socket ID:', self.socketId, '[Test]', args, file=sys.stderr)
-    print('-----------------------------', file=sys.stderr)
+    print(bcolors.PURPLE + '-----------------------------', file=sys.stderr)
+    print(bcolors.PURPLE + 'Name Space:', self.path, '[Connected]', file=sys.stderr)
+    print(bcolors.PURPLE + '-----------------------------', file=sys.stderr)
+    self.emit('new_connection', { 'name': self.path })
+  # def on_test(self, *args):
+  #   print(bcolors.BLUE + '-----------------------------', file=sys.stderr)
+  #   print(bcolors.BLUE + 'Name Space:', self.path, '[Test]', args, file=sys.stderr)
+  #   print(bcolors.BLUE + '-----------------------------', file=sys.stderr)
   def on_unique_event_response(self, *args):
-    print('-----------------------------', file=sys.stderr)
-    print('Socket ID:', self.socketId, 'UNIQUE EVENT', args, file=sys.stderr)
-    print('-----------------------------', file=sys.stderr)
+    print(bcolors.BLUE + '-----------------------------', file=sys.stderr)
+    print(bcolors.BLUE + 'Name Space:', self.path, 'UNIQUE EVENT', args, file=sys.stderr)
+    print(bcolors.BLUE + '-----------------------------', file=sys.stderr)
   def on_new_connection_recieved(self, *args):
-    print('-----------------------------', file=sys.stderr)
-    print('Socket ID:', self.socketId, '[A new socket was opened]', args, file=sys.stderr)
-    print('-----------------------------', file=sys.stderr)
-    self.emit('unique_event', { 'name': self.socketId })
-    #event only to people in name space 
+    print(bcolors.GREEN + '-----------------------------', file=sys.stderr)
+    print(bcolors.GREEN + 'Name Space:', self.path, '[A new socket was opened]', args, file=sys.stderr)
+    print(bcolors.GREEN + '-----------------------------', file=sys.stderr)
+    self.emit('unique_event', { 'name': self.path })
 
 class NamespaceTwo(BaseNamespace):
   def on_connect(self):
-    self.socketId = 'NameSpace2',
-    print('-----------------------------', file=sys.stderr)
-    print('Socket ID:', self.socketId, '[Connected]', file=sys.stderr)
-    print('-----------------------------', file=sys.stderr)
-    self.emit('new_connection', { 'name': self.socketId })
-  def on_test_event_from_node(self, *args):
-    print('-----------------------------', file=sys.stderr)
-    print('Socket ID: ', self.socketId, '(For All Clients)[Recieved test_event_from_node]', args, file=sys.stderr)
-    print('-----------------------------', file=sys.stderr)
-  # def on_test(self, *args):
-  #   print('-----------------------------', file=sys.stderr)
-  #   print('Socket ID:', self.socketId, '[Test]', args, file=sys.stderr)
-  #   print('-----------------------------', file=sys.stderr)
+    print(bcolors.PURPLE + '-----------------------------', file=sys.stderr)
+    print(bcolors.PURPLE + 'Name Space:', self.path, '[Connected]', file=sys.stderr)
+    print(bcolors.PURPLE + '-----------------------------', file=sys.stderr)
+    self.emit('new_connection', { 'name': self.path })
   def on_new_connection_recieved(self, *args):
-    print('-----------------------------', file=sys.stderr)
-    print('Socket ID:', self.socketId, '[A new socket was opened]', args, file=sys.stderr)
-    print('-----------------------------', file=sys.stderr)
-    #event only to people in name space 
+    print(bcolors.GREEN + '-----------------------------', file=sys.stderr)
+    print(bcolors.GREEN + 'Name Space:', self.path, '[A new socket was opened]', args, file=sys.stderr)
+    print(bcolors.GREEN + '-----------------------------', file=sys.stderr)
+
 
 @app.route('/')
 def hello_world():
@@ -72,24 +61,17 @@ def createSocketConnection(Namespace):
   t.daemon = True
   t.start()
 
-# @app.route('/test')
-# def test():
-#   print('hit test route', file=sys.stderr)
-#   socketIO.emit('test', 'testing')
-#   return 'test'
-
 if __name__ == '__main__':
-  for i in range(0, 2):
+  
+  createSocketConnection(NamespaceTwo)
+  for i in range(0, 3):
     #event only to people in name space 
-    if(i == 1):
-        socketIO = SocketIO('10.1.133.238', 140)
-        namespaceOne = socketIO.define(NamespaceOne, '/robot1')
-        t = threading.Thread(target=socketIO.wait)
-        t.daemon = True
-        t.start()
-    else:
-      createSocketConnection(NamespaceTwo)
-
+    name = '/robot'+str(i)
+    socketIO = SocketIO('10.1.133.238', 140)
+    namespaceOne = socketIO.define(NamespaceOne, name)
+    t = threading.Thread(target=socketIO.wait)
+    t.daemon = True
+    t.start()
 
   app.run(host='0.0.0.0', port=9000, debug=True)
 
