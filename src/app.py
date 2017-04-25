38fe56d5c95158eb9ba05e5a95eea95c599e5910
Samantha Vinci <samanthavinci@Samanthas-MBP.hellobond.io>
2017-04-25 14:58:48 -0400

    made func

--- src/app.py
+++ src/app.py
@@ -13,33 +13,59 @@ import threading
 
 app = Flask(__name__)
 
-class Namespace(BaseNamespace):
-    def on_connect(self):
-      self.socketId = random.randrange(0, 10000),
-      print('-----------------------------', file=sys.stderr)
-      print('Socket ID:', self.socketId, '[Connected]', file=sys.stderr)
-      print('-----------------------------', file=sys.stderr)
-    def on_test_event_from_node(self, *args):
-      print('-----------------------------', file=sys.stderr)
-      print('Socket ID:', self.socketId,'[Recieved test_event_from_node]', args, file=sys.stderr)
-      print('-----------------------------', file=sys.stderr)
-      self.emit('new_connection', { 'name': self.socketId })
-    # def on_test(self, *args):
-    #   print('-----------------------------', file=sys.stderr)
-    #   print('Socket ID:', self.socketId, '[Test]', args, file=sys.stderr)
-    #   print('-----------------------------', file=sys.stderr)
-    def new_connection_recieved(self, *args):
-      print('-----------------------------', file=sys.stderr)
-      print('Socket ID:', self.socketId, '[A new socket was opened]', args, file=sys.stderr)
-      print('-----------------------------', file=sys.stderr)
-      #event only to people in name space 
 
+class NamespaceOne(BaseNamespace):
+  def on_connect(self):
+    self.socketId = 'NameSpace1',
+    print('-----------------------------', file=sys.stderr)
+    print('Socket ID:', self.socketId, '[Connected]', file=sys.stderr)
+    print('-----------------------------', file=sys.stderr)
+  def on_test_event_from_node(self, *args):
+    print('-----------------------------', file=sys.stderr)
+    print('Socket ID: ', self.socketId, '(For All Clients)[Recieved test_event_from_node]', args, file=sys.stderr)
+    print('-----------------------------', file=sys.stderr)
+    self.emit('new_connection', { 'name': self.socketId })
+  # def on_test(self, *args):
+  #   print('-----------------------------', file=sys.stderr)
+  #   print('Socket ID:', self.socketId, '[Test]', args, file=sys.stderr)
+  #   print('-----------------------------', file=sys.stderr)
+  def new_connection_recieved(self, *args):
+    print('-----------------------------', file=sys.stderr)
+    print('Socket ID:', self.socketId, '[A new socket was opened]', args, file=sys.stderr)
+    print('-----------------------------', file=sys.stderr)
+    #event only to people in name space 
+
+class NamespaceTwo(BaseNamespace):
+  def on_connect(self):
+    self.socketId = 'NameSpace2',
+    print('-----------------------------', file=sys.stderr)
+    print('Socket ID:', self.socketId, '[Connected]', file=sys.stderr)
+    print('-----------------------------', file=sys.stderr)
+  def on_test_event_from_node(self, *args):
+    print('-----------------------------', file=sys.stderr)
+    print('Socket ID: ', self.socketId, '(For All Clients)[Recieved test_event_from_node]', args, file=sys.stderr)
+    print('-----------------------------', file=sys.stderr)
+    self.emit('new_connection', { 'name': self.socketId })
+  # def on_test(self, *args):
+  #   print('-----------------------------', file=sys.stderr)
+  #   print('Socket ID:', self.socketId, '[Test]', args, file=sys.stderr)
+  #   print('-----------------------------', file=sys.stderr)
+  def new_connection_recieved(self, *args):
+    print('-----------------------------', file=sys.stderr)
+    print('Socket ID:', self.socketId, '[A new socket was opened]', args, file=sys.stderr)
+    print('-----------------------------', file=sys.stderr)
+    #event only to people in name space 
 
 @app.route('/')
 def hello_world():
   print('hit home route', file=sys.stderr)
   return 'home'
 
+def createSocketConnection(Namespace):
+  socketIO = SocketIO('10.1.133.238', 140, Namespace)
+  t = threading.Thread(target=socketIO.wait)
+  t.daemon = True
+  t.start()
 # @app.route('/test')
 # def test():
 #   print('hit test route', file=sys.stderr)
@@ -48,10 +74,12 @@ def hello_world():
 
 if __name__ == '__main__':
   for i in range(0, 2):
-    socketIO = SocketIO('10.1.133.238', 140, Namespace)
-    t = threading.Thread(target=socketIO.wait)
-    t.daemon = True
-    t.start()
+    #event only to people in name space 
+    if(i == 1):
+      createSocketConnection(NamespaceOne)
+    else:
+      createSocketConnection(NamespaceTwo)
+
 
   app.run(host='0.0.0.0', port=9000, debug=True)
 
